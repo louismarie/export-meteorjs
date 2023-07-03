a9fc2e9073dfeb607703895f5c2ad619c2988a32
louismarie <dev@MacBook-Pro-de-Admin-4.local>
2023-07-03 16:59:54 +0200

    feat: reduce pooling interval

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,5 @@
 import { Export } from '../both/collections';
 
 Meteor.publish('exports', function() {
-    return Export.find({ ownerId: Meteor.userId()});
+    return Export.find({ ownerId: Meteor.userId()}, { pollingIntervalMs: 1000});
 });
